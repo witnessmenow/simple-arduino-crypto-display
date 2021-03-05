e8a0066d36a85a9c702dfd4df90310de1a9153d7
Piotr Wawrzynkiewicz <pio.wawrzynkiewicz@gmail.com>
2021-03-05 19:08:42 +0100

    Updated the software to work with coingecko API + bunch of UI changes

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # Simple Arduino Crypto Display
 
-### NOTE: Coinmarket cap have changed their API and this project currently does not work with it
+## 2021 UPDATE
 
-![alt text](https://i.imgur.com/QcoTabI.png "Display")
+![Crypto watch preview](https://user-images.githubusercontent.com/42513971/110156423-3977c500-7de7-11eb-9b7a-e87b180b8944.png)
+
+![alt text](https://i.imgur.com/QcoTabI.png 'Display')
 
 [![Alt text](https://img.youtube.com/vi/2IRsOFaWfho/0.jpg)](https://www.youtube.com/watch?v=2IRsOFaWfho)
 
@@ -13,39 +15,39 @@ A display built with an ESP8266 that fetches live currency prices directly from 
 I've tried to make this project as easy to make as possible. There is no soldering and you also do not need to know how to code. In total it should take 10-15 minutes. The total cost is also only about $8 or so.
 
 Crypto Wallets (If you feel like tipping, obviously no need to!)
-* ETH: 0x8b4334dc87d2231745772DBaDdC5FE8E705613BE
-* BTC: 14pzPrjedUyYb4iqxNA8xCPVKPcDKgeV1x
+
+- ETH: 0x8b4334dc87d2231745772DBaDdC5FE8E705613BE
+- BTC: 14pzPrjedUyYb4iqxNA8xCPVKPcDKgeV1x
 
 ### Parts Required
 
-* [NodeMCU ESP8266 Board](http://s.click.aliexpress.com/e/ayNNnYN)
-* [OLED Display 1.3" version](http://s.click.aliexpress.com/e/EqByrzb)
-* [Female-Female Dupont cables](http://s.click.aliexpress.com/e/2nq7AuR)
-
+- [NodeMCU ESP8266 Board](http://s.click.aliexpress.com/e/ayNNnYN)
+- [OLED Display 1.3" version](http://s.click.aliexpress.com/e/EqByrzb)
+- [Female-Female Dupont cables](http://s.click.aliexpress.com/e/2nq7AuR)
 
 ### 3D printed case from video:
+
 [Thingiverse](https://www.thingiverse.com/thing:2539435)
 
-
-### Wiring 
+### Wiring
 
 It should be wired as followed
 
-| Pin On Display| Pin on NodeMCU |
-| ------------- |----------------|
-| GND     | G |
-| VCC     | 3V (some boards say 3.3V |
-| SCL (Sometimes SCK)    | D5 |
-| SDA     | D3 |
+| Pin On Display      | Pin on NodeMCU           |
+| ------------------- | ------------------------ |
+| GND                 | G                        |
+| VCC                 | 3V (some boards say 3.3V |
+| SCL (Sometimes SCK) | D5                       |
+| SDA                 | D3                       |
 
 ---
 
 NOTE: Some displays have GND and VCC swapped around, always wire as above!
 
 ---
-![alt text](https://i.imgur.com/jaC6E2S.jpg?1 "Warning")
 
+![alt text](https://i.imgur.com/jaC6E2S.jpg?1 'Warning')
 
-![alt text](https://i.imgur.com/OrTd5SX.jpg "Wiring")
-![alt text](https://i.imgur.com/0pLIPvH.png "Wiring to Screen")
-![alt text](https://i.imgur.com/xgvBrwZ.png "Wiring to Board")
+![alt text](https://i.imgur.com/OrTd5SX.jpg 'Wiring')
+![alt text](https://i.imgur.com/0pLIPvH.png 'Wiring to Screen')
+![alt text](https://i.imgur.com/xgvBrwZ.png 'Wiring to Board')
--- SimpleCrypto/SimpleCrypto.ino
+++ SimpleCrypto/SimpleCrypto.ino
@@ -1,22 +1,40 @@
+#define ARDUINOJSON_USE_DOUBLE 1
+#define ARDUINOJSON_USE_LONG_LONG 1
+
 #include <ESP8266WiFi.h>
-#include <WiFiClientSecure.h>
+#include <ESP8266HTTPClient.h>
+#include <WifiClientSecure.h>
+#include <ArduinoJson.h>
 #include <Wire.h>
-#include "SH1106.h"
+#include "SH1106.h
+#include "cryptos.h"
 
-char ssid[] = "WiFiNetworkName";
-char password[] = "password";
+// ----------------------------
+// Configurations - Update these
+// ----------------------------
 
-// Pins based on your wiring
+const char* ssid = "YOUR_WIFI_SSID";
+const char* password = "YOUR_WIFI_PASSWORD";
+unsigned long screenChangeDelay = 10000;
+
 #define SCL_PIN D5
 #define SDA_PIN D3
 
-unsigned long screenChangeDelay = 10000;
+// ----------------------------
+// End of area you need to change
+// ----------------------------
+
 SH1106 display(0x3c, SDA_PIN, SCL_PIN);
+unsigned long screenChangeDue;
+int cryptosCount = (sizeof(cryptos) / sizeof(cryptos[0]));
 
+void renderSimpleText(String text) {
+  display.clear();
+  display.drawString(64, 18, text);
+  display.display();
+}
 
-void setup() {
-  Serial.begin(115200);
-  display.init();
+void renderWelcomeMsg() {
   display.setTextAlignment(TEXT_ALIGN_CENTER);
   display.setFont(ArialMT_Plain_16);
   display.drawString(64, 0, F("HODL Display"));
@@ -26,6 +44,149 @@ void setup() {
   display.display();
 }
 
-void loop() {
+void connect() {
+  WiFi.begin(ssid, password);
+  String dots[3] = {".", "..", "..."};
+  int numberOfDots = 1;
+  
+  while (WiFi.status() != WL_CONNECTED) 
+  {
+    display.clear();
+    display.setTextAlignment(TEXT_ALIGN_CENTER);
+    display.setFont(ArialMT_Plain_10);
+    display.drawString(64, 18, "Connecting to WiFi " + dots[numberOfDots]);
+    display.display();
 
+    if (numberOfDots == 3) {
+      numberOfDots = 0;
+    } else {
+      numberOfDots++;
+    }
+
+    delay(300);
+  }
+
+   renderSimpleText("Connected.");
+}
+
+String createApiUrl(String vsCurrency) {
+  String cryptosString = "";
+
+  for (int i = 0; i < cryptosCount; i++) {
+    cryptosString += cryptos[i].apiName + ",";
+  }
+
+  return "https://api.coingecko.com/api/v3/coins/markets?vs_currency=" + vsCurrency + "&ids=" + cryptosString + "&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=24h%2C7d";
+}
+
+int getCryptoIndexById(String id) {
+  for (int i = 0; i < cryptosCount; i++) {
+    if (cryptos[i].apiName == id) return i;
+  }
+}
+
+void downloadData(String vsCurrency) {
+  HTTPClient http;          
+  WiFiClientSecure client;
+  client.setInsecure(); 
+
+  String apiUrl = createApiUrl(vsCurrency);
+  
+  client.connect(apiUrl, 443);
+  http.begin(client, apiUrl);
+
+  String response;
+  if (http.GET() == HTTP_CODE_OK) {
+    response = http.getString();  
+  } else {
+    renderSimpleText("Error connecting to API");
+    return;
+  }
+
+  DynamicJsonDocument doc(4000);
+  DeserializationError error = deserializeJson(doc, response);
+  
+  if (error) {
+    Serial.print(F("deserializeJson() failed: "));
+    Serial.println(error.f_str());
+    renderSimpleText("JSON deserialization error");
+    return;
+  }
+
+  for (int i = 0; i < cryptosCount; i++) {
+    JsonObject json = doc[i];
+    String id = json["id"];
+    int cryptoIndex = getCryptoIndexById(id);
+    String symbol = json["symbol"];
+    symbol.toUpperCase();
+    double currentPrice = json["current_price"];
+    double dayChange = json["price_change_percentage_24h_in_currency"];
+    double weekChange = json["price_change_percentage_7d_in_currency"];
+    
+    cryptos[cryptoIndex].symbol = symbol;
+    cryptos[cryptoIndex].priceUsd = currentPrice;
+    cryptos[cryptoIndex].dayChange = dayChange;
+    cryptos[cryptoIndex].weekChange = weekChange;
+  }
+ 
+}
+
+String formatCurrency(float price) {
+  int digitsAfterDecimal = 3;
+  
+  if (price >= 1000) {
+    digitsAfterDecimal = 0;
+  } else if (price >= 100) {
+    digitsAfterDecimal = 1;
+  } else if (price >= 1) {
+    digitsAfterDecimal = 2;
+  } else if (price < 0.001) {
+    digitsAfterDecimal = 4;
+  } 
+  
+  return String(price, digitsAfterDecimal);
+}
+
+void renderCryptoLogo(Crypto crypto) {  
+  int offX = 90;
+  int offY = 4;
+  int width = 32;
+  int height = 32;
+  
+  display.drawXbm(offX, offY, width, height, (uint8_t*)( crypto.bitmap ));
+}  
+
+void renderCrypto(Crypto crypto) {
+  display.clear();
+  renderCryptoLogo(crypto);
+  display.setTextAlignment(TEXT_ALIGN_LEFT);
+  display.setFont(ArialMT_Plain_16);
+  display.drawString(90, 45, crypto.symbol);
+  display.drawString(0, 10, "$");
+  display.setFont(ArialMT_Plain_24);
+  display.drawString(10, 6, formatCurrency(crypto.priceUsd));
+  display.setFont(ArialMT_Plain_10);
+  display.drawString(0, 42, "24h:  " + String(crypto.dayChange) + "%");
+  display.drawString(0, 54, "7d:  " + String(crypto.weekChange) + "%");
+  display.display();
+}
+
+
+
+
+void setup() {
+  Serial.begin(115200);
+  display.init();
+  renderWelcomeMsg();
+  delay(4000);
+  connect();
+}
+
+void loop() {
+  downloadData("usd");
+    
+  for (int i = 0; i < cryptosCount; i++) {
+    renderCrypto(cryptos[i]);
+    delay(screenChangeDelay);
+  }
 }
